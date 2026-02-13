7a15a37c136c5d4cedd81e1b07b68d7b7d7b3689
savadah <syrovkirill0@gmail.com>
2026-02-13 15:54:28 +0300

    Task18: 2 - каркас MyTreeMap и сравнение ключей

--- Tasks/Task18/Task18/Program.cs
+++ Tasks/Task18/Task18/Program.cs
@@ -1,15 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Task18
+class Program
 {
-    internal class Program
+       static void Main()
     {
-        static void Main(string[] args)
+        
+    }
+
+    public class MyTreeMap<K, V>
+    {
+        private readonly IComparer<K> comparator;
+        private Node root;
+        private int size;
+
+        private class Node
         {
+            public K Key;
+            public V Value;
+            public Node Left;
+            public Node Right;
+
+            public Node(K key, V value)
+            {
+                Key = key;
+                Value = value;
+                Left = null;
+                Right = null;
+            }
+        }
+
+        public MyTreeMap()
+        {
+            comparator = null;
+            root = null;
+            size = 0;
+        }
+
+        public MyTreeMap(IComparer<K> comp)
+        {
+            if (comp == null) throw new ArgumentNullException("comp");
+            comparator = comp;
+            root = null;
+            size = 0;
+        }
+
+        private int CompareKeys(K a, K b)
+        {
+            if (object.Equals(a, null)) throw new ArgumentNullException("a");
+            if (object.Equals(b, null)) throw new ArgumentNullException("b");
+
+            if(comparator != null)
+            {
+                return comparator.Compare(a, b);
+            }
+
+            IComparable<K> cmp = a as IComparable<K>;
+            if (cmp != null)
+            {
+                return cmp.CompareTo(b);
+            }
+            else throw new InvalidOperationException("Тип ключа " + typeof(K).Name + " не поддерживает сравнение");
         }
     }
 }
