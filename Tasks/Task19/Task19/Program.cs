1a78bc206c2a027397ee643967ece81a72d8023f
savadah <syrovkirill0@gmail.com>
2026-03-24 18:48:56 +0300

    Task19: 2 - Каркас MyTreeMap + узел дерева + сравнение ключей

--- Tasks/Task19/Task19/Program.cs
+++ Tasks/Task19/Task19/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Task19
 {
@@ -10,6 +7,81 @@ namespace Task19
     {
         static void Main(string[] args)
         {
+        }
+    }
+
+    public class MyTreeMap<K, V>
+    {
+        private Node root;
+        private int size;
+        private IComparer<K> comparator;
+
+        private class Node
+        {
+            public K Key;
+            public V Value;
+            public Node Left;
+            public Node Right;
+            public Node Parent;
+            public bool IsRed;
+
+            public Node(K key, V value, Node parent)
+            {
+                Key = key;
+                Value = value;
+                Parent = parent;
+                Left = null;
+                Right = null;
+                IsRed = true;
+            }
+        }
+
+        //1
+        public MyTreeMap()
+        {
+            root = null;
+            size = 0;
+            comparator = null;
+        }
+
+        //2
+        public MyTreeMap(IComparer<K> comp)
+        {
+            if (comp == null)
+            {
+                throw new ArgumentNullException("comp");
+            }
+
+            root = null;
+            size = 0;
+            comparator = comp;
+        }
+
+        public int CompareKeys(K a, K b)
+        {
+            if (object.Equals(a, null))
+            {
+                throw new ArgumentNullException("a");
+            }
+
+            if (object.Equals(b, null))
+            {
+                throw new ArgumentNullException("b");
+            }
+
+            if (comparator != null)
+            {
+                return comparator.Compare(a, b);
+            }
+
+            IComparable<K> cmp = a as IComparable<K>;
+
+            if (cmp != null)
+            {
+                return cmp.CompareTo(b);
+            }
+
+            throw new InvalidOperationException("Тип ключа не поддерживает сравнение");
         }
     }
 }
