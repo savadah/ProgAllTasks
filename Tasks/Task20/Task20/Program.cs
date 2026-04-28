23c13608d8dd2adc783a134a96c76e34bb84fa35
savadah <syrovkirill0@gmail.com>
2026-04-28 18:34:32 +0300

    Task20: 4 — структура транспортной сети

--- Tasks/Task20/Task20/Program.cs
+++ Tasks/Task20/Task20/Program.cs
@@ -3,6 +3,25 @@ using System.Collections.Generic;
 
 class Program
 {
+    class Edge
+    {
+        public int To;
+        public int Reverse;
+        public int Capacity;
+        public int Flow;
+
+        public Edge(int to, int reverse, int capacity)
+        {
+            To = to;
+            Reverse = reverse;
+            Capacity = capacity;
+            Flow = 0;
+        }
+    }
+
+    static List<Edge>[] net;
+    static int nNet;
+
     static void Main(string[] args)
     {
         Task1();
@@ -91,6 +110,45 @@ class Program
     static void Task10()
     {
         Console.WriteLine("10. Построение максимального потока. Алгоритм Эдмондса-Карпа");
+
+        nNet = 4;
+
+        net = new List<Edge>[nNet];
+
+        for (int i = 0; i < nNet; i++)
+        {
+            net[i] = new List<Edge>();
+        }
+
+        AddEdge(0, 1, 3);
+        AddEdge(0, 2, 2);
+        AddEdge(1, 2, 1);
+        AddEdge(1, 3, 2);
+        AddEdge(2, 3, 4);
+
+        Console.WriteLine("Транспортная сеть:");
+
+        for (int i = 0; i < nNet; i++)
+        {
+            for (int j = 0; j < net[i].Count; j++)
+            {
+                Edge e = net[i][j];
+
+                if (e.Capacity > 0)
+                {
+                    Console.WriteLine((i + 1) + " -> " + (e.To + 1) + ", пропускная способность = " + e.Capacity);
+                }
+            }
+        }
+    }
+
+    static void AddEdge(int from, int to, int capacity)
+    {
+        Edge direct = new Edge(to, net[to].Count, capacity);
+        Edge back = new Edge(from, net[from].Count, 0);
+
+        net[from].Add(direct);
+        net[to].Add(back);
     }
 
     static void Task16()
