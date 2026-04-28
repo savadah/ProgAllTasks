065ef426bf128e32c572437c1cea5593eff28830
savadah <syrovkirill0@gmail.com>
2026-04-28 17:50:38 +0300

    Task20: 2 — каркас программы для трех задач

--- Tasks/Task20/Task20/Program.cs
+++ Tasks/Task20/Task20/Program.cs
@@ -1,15 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Task20
+class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-        }
+        Task1();
+
+        Console.WriteLine();
+
+        Task10();
+
+        Console.WriteLine();
+
+        Task16();
+
+        Console.ReadLine();
+    }
+
+    static void Task1()
+    {
+        Console.WriteLine("1. Построение транзитивного замыкания с помощью DFS");
+    }
+
+    static void Task10()
+    {
+        Console.WriteLine("10. Построение максимального потока. Алгоритм Эдмондса-Карпа");
+    }
+
+    static void Task16()
+    {
+        Console.WriteLine("16. Поиск шарниров в графе");
     }
 }
