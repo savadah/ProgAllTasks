be4bf6be1b548ec7d3f6354a7d22529df0ca319e
savadah <syrovkirill0@gmail.com>
2026-04-28 18:28:21 +0300

    Task20: 3 — транзитивное замыкание через DFS

--- Tasks/Task20/Task20/Program.cs
+++ Tasks/Task20/Task20/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 class Program
 {
@@ -20,6 +21,71 @@ class Program
     static void Task1()
     {
         Console.WriteLine("1. Построение транзитивного замыкания с помощью DFS");
+
+        int n = 4;
+
+        List<int>[] graph = new List<int>[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            graph[i] = new List<int>();
+        }
+
+        graph[0].Add(1);
+        graph[1].Add(2);
+        graph[3].Add(1);
+
+        bool[,] close = new bool[n, n];
+
+        for (int i = 0; i < n; i++)
+        {
+            bool[] mark = new bool[n];
+
+            for (int j = 0; j < graph[i].Count; j++)
+            {
+                int to = graph[i][j];
+
+                if (mark[to] == false)
+                {
+                    DfsClose(i, to, graph, mark, close);
+                }
+            }
+        }
+
+        Console.WriteLine("Матрица транзитивного замыкания:");
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (close[i, j] == true)
+                {
+                    Console.Write("1 ");
+                }
+                else
+                {
+                    Console.Write("0 ");
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+    static void DfsClose(int start, int v, List<int>[] graph, bool[] mark, bool[,] close)
+    {
+        mark[v] = true;
+        close[start, v] = true;
+
+        for (int i = 0; i < graph[v].Count; i++)
+        {
+            int to = graph[v][i];
+
+            if (mark[to] == false)
+            {
+                DfsClose(start, to, graph, mark, close);
+            }
+        }
     }
 
     static void Task10()
